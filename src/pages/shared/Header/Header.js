19f63e594a9645b28d79a30568609a881ef02e83
mrc4619 <mrchayon5525@gmail.com>
2022-11-07 23:05:22 +0600

    footer added

--- src/pages/shared/Header/Header.js
+++ src/pages/shared/Header/Header.js
@@ -20,7 +20,12 @@ const Header = () => {
                             {navItem}
                         </ul>
                     </div>
-                    <Link className="btn btn-ghost normal-case text-xl"> ExPhotography</Link>
+                    <Link className="btn btn-ghost normal-case text-xl">
+                        <span className="flex items-center justify-center mr-2 w-8 h-8 rounded-full bg-gray-100">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" fill="currentColor" className="flex-shrink-0 w-5 h-5 rounded-full text-gray-900">
+                                <path d="M18.266 26.068l7.839-7.854 4.469 4.479c1.859 1.859 1.859 4.875 0 6.734l-1.104 1.104c-1.859 1.865-4.875 1.865-6.734 0zM30.563 2.531l-1.109-1.104c-1.859-1.859-4.875-1.859-6.734 0l-6.719 6.734-6.734-6.734c-1.859-1.859-4.875-1.859-6.734 0l-1.104 1.104c-1.859 1.859-1.859 4.875 0 6.734l6.734 6.734-6.734 6.734c-1.859 1.859-1.859 4.875 0 6.734l1.104 1.104c1.859 1.859 4.875 1.859 6.734 0l21.307-21.307c1.859-1.859 1.859-4.875 0-6.734z"></path>
+                            </svg>
+                        </span>  ExPhotography</Link>
                 </div>
                 <div className="navbar-center hidden lg:flex">
                     <ul className="menu menu-horizontal p-0">
